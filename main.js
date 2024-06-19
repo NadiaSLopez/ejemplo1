c76c5cf7548bfa7645805c2c4d5332560f32f6d0
NadiaSLopez <nadiam.s.lopez@gmail.com>
2024-06-18 19:53:05 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+// console.log("Hola mundo");
+
+//forma 1 de declarar variable
+//let a = 10, 
+//let b = 10;
+//let c = 10;
+//let d = 10;
+//let x = a + b;
+
+//console.log(x);
+
+//Forma 2 de declarar variable abreviada
+//let a = 10, b= 20, c = 10, d = 10, x = a + b;
+
+
+
+
+
+
+
+//console.log( "a", a);
+//console.log("b", b);
+//console.log("c", c);
+
+//console.log( "%c Mis variables", "color:blue; font-weight: bold");
+//console.log({ a });
+//console.log({ b });
+//console.log({ c });
+
+
+
+//const c = 10;
+//constante es un valor y no cambia
+
+//TIPOS console.log(x);
+//console.warn(x);
+//console.error(x);
+
+
+
+let a = 10, 
+    b = 20;
+    c = "Hola";
+    d = "Spiderman"; 
+    x = a + b;
+
+console.table({a,b,c,d,x});
---
+++ primitivos.js
@@ -0,0 +1,36 @@
+let nombre= "peter parket";
+console.log(nombre);
+
+ nombre = "Ben Parker";
+ console.log(nombre);
+
+ nombre = "Tia May";
+ 
+ nombre = "Tia May";
+
+ console.log (typeof nombre);
+
+
+
+ let esMarvel = true;
+ console.log (typeof esMarvel);
+
+ edad = 33;
+ console.log(typeof edad);
+
+
+ let superPoder;
+
+ console.log(typeof superPoder);
+
+
+ let soyNull = null;
+ console.log(typeof soyNull);
+
+
+ let symbol1 = Symbol();
+ let symbol2 = Symnol();
+
+ console.log(typeof symbol1);
+
+
